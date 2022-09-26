a2dc6233199927a645ac17663e620b5f7b663340
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-26 00:12:56 -0300

    criando no mongodb

---
+++ src/database/UserSchema.ts
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+
+const UserModel = mongoose.model("User",{
+ 
+    name:{
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+});
+
+
+export { UserModel }
--- src/routes/users.routers.ts
+++ src/routes/users.routers.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { UserModel } from "../database/UserSchema";
 import { UsersRepository } from "../modules/user/repositories/implemantations/userRepository";
 import { createUserController } from "../modules/user/useCases/createUser";
 import { deleteUserController } from "../modules/user/useCases/deleteUser";
@@ -17,15 +18,20 @@ usersRoutes.get("/", (req, res)=>{
     return createUserController.handle(req, res);
  });
 
-//  usersRoutes.get("/user", (req, res)=>{
+  usersRoutes.post("/user", async (req, res)=>{
  
-//     const { name } = req.body.user
+    const user = req.body.user
  
-//     const userFind = userReposiotry.getUserByName(name)
+    try{
+      await UserModel.create(user)
+
+      res.sendStatus(201)
+
+    } catch(errp){
+      console.log(errp)
+    }
  
-//     res.json(userFind)
- 
-//  });
+ });
  
 
  usersRoutes.put("/upd", (req, res)=>{
